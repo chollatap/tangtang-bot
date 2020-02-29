19b3f7ec88d964eefc6dc02ea0e4fbd9b0794659
chollatap <31573478+chollatap@users.noreply.github.com>
2020-03-01 04:10:45 +0700

    Update line.php

--- line.php
+++ line.php
@@ -14,7 +14,7 @@ function send_LINE($msg){
       $url = 'https://api.line.me/v2/bot/message/push';
       $data = [
 
-        'to' => 'chollatap',
+        'to' => '1531979031',
         'messages' => [$messages],
       ];
       $post = json_encode($data);
