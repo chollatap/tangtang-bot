e9ee5b03dce177f5e2134bba6079d25c5f555301
chollatap <31573478+chollatap@users.noreply.github.com>
2020-03-01 03:51:03 +0700

    Update callback.php

--- callback.php
+++ callback.php
@@ -4,7 +4,7 @@ $access_token = 'XIGfeOi56hMqF/zd60jKU5wT6b8ADXtLGV6ulG9V5LOJbkk026aK48Ue2kmozKA
 // Get POST body content
 
 
-$url = 'https://api.line.me/v2/bot/message/push';
+$url = ' https://api.line.me/v2/oauth/verify';
 
 $headers = array('Authorization: Bearer ' . $access_token);
 
