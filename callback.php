79ae64697964956cedb9ab214d8e5d182cca0a36
chollatap <31573478+chollatap@users.noreply.github.com>
2020-03-01 04:11:54 +0700

    Update callback.php

--- callback.php
+++ callback.php
@@ -1,18 +1,1 @@
-<?php // callback.php
-
-$access_token = 'XIGfeOi56hMqF/zd60jKU5wT6b8ADXtLGV6ulG9V5LOJbkk026aK48Ue2kmozKABc86eBm8pucaoN1FJBiZC11fnzHSK83kspjFmar8dwu1YbRbb4SyZ9LZDnumcTmUuJnQ1fqlkgsEM6O+5MqoAAwdB04t89/1O/w1cDnyilFU=';
-// Get POST body content
-
-
-$url = ' https://api.line.me/v2/oauth/verify';
-
-$headers = array('Authorization: Bearer ' . $access_token);
-
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-$result = curl_exec($ch);
-curl_close($ch);
-
-echo $result;
+curl -v -X POST https://api.line.me/v2/oauth/verify \-H 'Content-Type: application/x-www-form-urlencoded' \--data-urlencode 'access_token='
