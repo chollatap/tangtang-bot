e555c17a7d337b24043a70901289677190f86b2d
chollatap <31573478+chollatap@users.noreply.github.com>
2020-03-24 18:19:14 +0700

    Create test.py

---
+++ test.py
@@ -0,0 +1,20 @@
+a = 0b111101
+b = 0b000010
+
+# print value in binary 
+print "values in binary..."
+print "a: ",bin (a)
+print "b: ",bin (b)
+
+# bitwise OR and AND operations
+print "(a|b) : ", bin (a|b)
+print "(a&b) : ", bin (a&b)
+
+# print values in decimal 
+print "values in decimal..."
+print "a: ",a 
+print "b: ",b 
+
+# bitwise OR and AND operations 
+print "(a|b) : ", int (bin (a|b),2)
+print "(a&b) : ", int (bin (a&b),2)
